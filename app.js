5d9a7e36f72a5e312b1bc2bb2cb9e0160d77d11a
aroma-RH <effectif19@gmail.com>
2025-03-10 19:14:51 +0000

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const boutonFermer = modal.querySelector('.close-button');
 let graphiqueStatut, graphiqueGroupe, graphiqueContrat;
 
 // Point de terminaison de l'API Google Sheets
-const URL_API_SHEETS = 'https://script.google.com/macros/s/AKfycbx1_L8ndzZd2W59PcCItx5jM8IsUM0Vs8_977JZm1I1ImncsTI7Q098CLjxK_fvHEWtiQ/exec';
+const URL_API_SHEETS = 'https://script.google.com/macros/s/AKfycbzpWGznMf1qKNyevSOyfs2n8TaUiO43wL_STwp8Eq92eVl1MK0XNYdd2bTWSKi_OHTelQ/exec';
 
 // État
 let travailleurs = [];
